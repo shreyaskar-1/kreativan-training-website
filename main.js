73026dccfc65f82cf9b7d431102f2cc4a3f3b9c5
shreyaskar-1 <shreyaskar_219031@gmail.com>
2024-02-15 16:01:30 +0530

    First Commit

---
+++ main.js
@@ -0,0 +1,27 @@
+var tl = gsap.timeline()
+tl.from("#md1", {
+    rotate: 2,
+    duration: 1,
+    yoyo: true,
+    repeat: -1
+})
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    function toggleSearchField() {
+        var searchField = document.getElementById("searchField");
+        searchField.classList.toggle("show");
+    }
+
+    const toggleBtn = document.querySelector(".toggle_btn");
+    const toggleBtnIcon = document.querySelector(".toggle_btn i");
+    const dropdownMenu = document.querySelector(".dropdown_menu");
+    toggleBtn.onclick = function () {
+        dropdownMenu.classList.toggle("open");
+        const isOpen = dropdownMenu.classList.contains("open");
+
+        toggleBtnIcon.classList = isOpen
+            ? "fa-solid fa-xmark"
+            : "fa-solid fa-bars";
+    };
+});
